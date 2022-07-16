fb92511173ad00e25b5da958aca4dd9fa7635e04
AndriP8 <andrikcoc26@gmail.com>
2022-07-16 16:54:41 +0700

    init ui member

---
+++ frontend/src/containers/members/MenuMember.tsx
@@ -0,0 +1,47 @@
+import { Box, HStack } from "@chakra-ui/react";
+
+const MenuMember = () => {
+  return (
+    <Box
+      position={"fixed"}
+      right={"0"}
+      left={"0"}
+      bottom={"0"}
+      backgroundColor={"white"}
+      py={"4"}
+    >
+      <HStack
+        justifyContent={"space-between"}
+        mx={"4"}
+        border={"1px"}
+        borderRadius={"16px"}
+        padding={"1"}
+        backgroundColor={"gray.700"}
+        position={"relative"}
+        maxW={"440px"}
+        py={"1"}
+      >
+        <Box
+          w={"50%"}
+          textAlign={"center"}
+          borderRadius={"12px"}
+          py={"4"}
+          backgroundColor={"gray.100"}
+        >
+          Members
+        </Box>
+        <Box
+          w={"50%"}
+          textAlign={"center"}
+          borderRadius={"16px"}
+          py={"4"}
+          color={"white"}
+        >
+          Bills
+        </Box>
+      </HStack>
+    </Box>
+  );
+};
+
+export default MenuMember;
