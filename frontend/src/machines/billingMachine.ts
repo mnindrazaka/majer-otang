f3b5f25724cae505fc8e000ae54984c3882ebeaf
dannydwic <danny.dwi@ruangguru.com>
2022-07-16 16:42:31 +0700

    init billing finite state machine flow

--- frontend/src/machines/billingMachine.ts
+++ frontend/src/machines/billingMachine.ts
@@ -1,3 +1,137 @@
-import { createMachine } from "xstate";
+import { createMachine, assign } from "xstate";
 
-export const billingMachine = createMachine({});
+type Billing = {
+  id: string;
+  title: string;
+  amount: number;
+};
+
+interface Context {
+  billings: Billing[];
+  isEditingForm: boolean;
+  isCreatingForm: boolean;
+}
+
+const billingMachine = createMachine<Context>({
+  id: "billing",
+  initial: "idle",
+  context: {
+    billings: [],
+    isCreatingForm: false,
+    isEditingForm: false,
+  },
+  states: {
+    idle: {
+      on: {
+        FETCHINGBILLINGS: {
+          target: "gettingBilling",
+        },
+      },
+    },
+    gettingBilling: {
+      on: {
+        FETCHINGBILLINGSSUCCES: {
+          target: "gettingBillingOK",
+        },
+        FETCHINGBILLINGSERROR: {
+          target: "gettingBillingError",
+        },
+      },
+    },
+    gettingBillingOK: {
+      on: {
+        ACTIVATEBILLINGFORM: {
+          target: "billingFormIdle",
+        },
+      },
+    },
+    gettingBillingError: {
+      on: {
+        REFETCHINGBILLINGS: {
+          target: "gettingBilling",
+        },
+      },
+    },
+    billingFormIdle: {
+      always: [
+        {
+          cond: "creatingForm",
+          target: "#firstStep",
+        },
+        {
+          target: "gettingBillingDetail",
+        },
+      ],
+    },
+    gettingBillingDetail: {
+      on: {
+        FETCHINGBILLINGDETAILSUCCES: {
+          target: "#firstStep",
+        },
+        FETCHINGBILLINGDETAILERROR: {
+          target: "gettingBillingDetailError",
+        },
+      },
+    },
+    gettingBillingDetailError: {
+      on: {
+        REFTECHBILLINGDETAIL: {
+          target: "gettingBillingDetail",
+        },
+      },
+    },
+    billingFormReady: {
+      initial: "firstStep",
+      states: {
+        firstStep: {
+          id: "firstStep",
+          on: {
+            NEXTSTEP: {
+              target: "secondStep",
+            },
+          },
+        },
+        secondStep: {
+          id: "secondStep",
+          on: {
+            PREVSTEP: {
+              target: "firstStep",
+            },
+            SUBMITBILLING: {
+              target: "#submittingBilling",
+            },
+          },
+        },
+      },
+    },
+    submittingBilling: {
+      id: "submittingBilling",
+      on: {
+        SUBMITBILLINGSUCCES: {
+          target: "submittingBillingOK",
+        },
+        SUBMITBILLINGERROR: {
+          target: "submittingBillingError",
+        },
+      },
+    },
+    submittingBillingOK: {
+      type: "final",
+      on: {
+        BACKTOBILLINGSSCREEN: {
+          target: "gettingBillingOK",
+        },
+      },
+    },
+    submittingBillingError: {
+      on: {
+        RETRYSUBMITBILLING: {
+          target: "submittingBilling",
+        },
+        BACKTOSECONDSTEPFORM: {
+          target: "#secondStep",
+        },
+      },
+    },
+  },
+});
