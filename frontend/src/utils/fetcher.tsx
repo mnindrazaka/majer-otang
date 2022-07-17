e46755790378245f80b282875b289f2c2ebce05c
M Nindra Zaka <mnindrazaka@gmail.com>
2022-07-17 14:55:57 +0700

    feat: generate fetcher utils on frontend (#11)
    * feat: generate fetcher utils on frontend
    
    * use single quote
    
    * add ErrorResponse
    
    Co-authored-by: M. Nindra Zaka <nindra.zaka@ruangguru.com>

---
+++ frontend/src/utils/fetcher.tsx
@@ -0,0 +1,9 @@
+import { Configuration, MembersApi } from '../__generated__/api';
+
+export * from '../__generated__/api';
+
+const configuration = new Configuration({
+  basePath: 'https://api.themoviedb.org/3',
+});
+
+export const membersApi = new MembersApi(configuration);
