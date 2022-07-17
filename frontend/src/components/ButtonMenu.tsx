989f0cab6ce0e801b9b1a7db11d9f6c38291f777
Andri purnomo <56879512+AndriP8@users.noreply.github.com>
2022-07-17 13:10:23 +0700

    Init UI member (#3)
    * init ui member
    
    * add missing style
    
    * add layout component
    
    * remove unused component
    
    * refactor structure members page
    
    * refactor menu button
    
    * update structure folder button menu

---
+++ frontend/src/components/ButtonMenu.tsx
@@ -0,0 +1,65 @@
+import { Box, HStack } from "@chakra-ui/react";
+
+interface MenuItemProps {
+  title: string;
+  isActive: boolean;
+}
+
+const MenuItem = (props: MenuItemProps) => {
+  return (
+    <Box
+      w={"50%"}
+      textAlign={"center"}
+      borderRadius={"12px"}
+      py={"4"}
+      cursor={"pointer"}
+      backgroundColor={props.isActive ? "gray.100" : "gray.700"}
+      color={props.isActive ? "gray.700" : "gray.100"}
+    >
+      {props.title}
+    </Box>
+  );
+};
+
+const menus = {
+  members: "Members",
+  billings: "Billings"
+};
+
+type ButtonMenuProps = {
+  activeMenu: keyof typeof menus;
+};
+
+const ButtonMenu = (props: ButtonMenuProps) => {
+  return (
+    <Box
+      position={"fixed"}
+      right={"0"}
+      left={"0"}
+      bottom={"0"}
+      backgroundColor={"white"}
+      py={"4"}
+      display={"flex"}
+      justifyContent={"center"}
+    >
+      <HStack
+        justifyContent={"space-between"}
+        mx={"4"}
+        border={"1px"}
+        borderRadius={"16px"}
+        padding={"1"}
+        backgroundColor={"gray.700"}
+        position={"relative"}
+        maxW={"440px"}
+        w={"full"}
+        py={"1"}
+      >
+        {Object.entries(menus).map(([id, title]) => (
+          <MenuItem key={id} title={title} isActive={props.activeMenu === id} />
+        ))}
+      </HStack>
+    </Box>
+  );
+};
+
+export default ButtonMenu;
---
+++ frontend/src/containers/MembersPage/CardMember.tsx
@@ -0,0 +1,111 @@
+import { Box, Text, VStack } from "@chakra-ui/react";
+
+const CardMember = () => {
+  return (
+    <Box m={"4"} color={"gray.200"} mb={"36"}>
+      <VStack spacing={"4"}>
+        <Box
+          border={"1px"}
+          borderRadius={"16px"}
+          p={"6"}
+          w={"full"}
+          backgroundColor={"gray.700"}
+        >
+          <Text fontSize={"2xl"} fontWeight={"semibold"}>
+            John Doe
+          </Text>
+        </Box>
+        <Box
+          border={"1px"}
+          borderRadius={"16px"}
+          p={"6"}
+          w={"full"}
+          backgroundColor={"gray.700"}
+        >
+          <Text fontSize={"2xl"} fontWeight={"semibold"}>
+            John Doe
+          </Text>
+        </Box>
+        <Box
+          border={"1px"}
+          borderRadius={"16px"}
+          p={"6"}
+          w={"full"}
+          backgroundColor={"gray.700"}
+        >
+          <Text fontSize={"2xl"} fontWeight={"semibold"}>
+            John Doe
+          </Text>
+        </Box>
+        <Box
+          border={"1px"}
+          borderRadius={"16px"}
+          p={"6"}
+          w={"full"}
+          backgroundColor={"gray.700"}
+        >
+          <Text fontSize={"2xl"} fontWeight={"semibold"}>
+            John Doe
+          </Text>
+        </Box>
+        <Box
+          border={"1px"}
+          borderRadius={"16px"}
+          p={"6"}
+          w={"full"}
+          backgroundColor={"gray.700"}
+        >
+          <Text fontSize={"2xl"} fontWeight={"semibold"}>
+            John Doe
+          </Text>
+        </Box>
+        <Box
+          border={"1px"}
+          borderRadius={"16px"}
+          p={"6"}
+          w={"full"}
+          backgroundColor={"gray.700"}
+        >
+          <Text fontSize={"2xl"} fontWeight={"semibold"}>
+            John Doe
+          </Text>
+        </Box>
+        <Box
+          border={"1px"}
+          borderRadius={"16px"}
+          p={"6"}
+          w={"full"}
+          backgroundColor={"gray.700"}
+        >
+          <Text fontSize={"2xl"} fontWeight={"semibold"}>
+            John Doe
+          </Text>
+        </Box>
+        <Box
+          border={"1px"}
+          borderRadius={"16px"}
+          p={"6"}
+          w={"full"}
+          backgroundColor={"gray.700"}
+        >
+          <Text fontSize={"2xl"} fontWeight={"semibold"}>
+            John Doe
+          </Text>
+        </Box>
+        <Box
+          border={"1px"}
+          borderRadius={"16px"}
+          p={"6"}
+          w={"full"}
+          backgroundColor={"gray.700"}
+        >
+          <Text fontSize={"2xl"} fontWeight={"semibold"}>
+            John Doe
+          </Text>
+        </Box>
+      </VStack>
+    </Box>
+  );
+};
+
+export default CardMember;
