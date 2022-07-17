1d9317f6414d86c28d57c116091cea403737827d
Deby Silvia Agnes <deby.silvia23@gmail.com>
2022-07-17 15:26:37 +0700

    feat: add route get members list

--- backend/handler/api/member-mapper.go
+++ backend/handler/api/member-mapper.go
@@ -1,8 +1,9 @@
 package api
 
 import (
-	"github.com/mnindrazaka/billing/utils"
 	"net/http"
+
+	"github.com/mnindrazaka/billing/utils"
 )
 
 func buildGetMemberByIDError(w http.ResponseWriter, err error) {
@@ -15,5 +16,18 @@ func buildGetMemberByIDError(w http.ResponseWriter, err error) {
 }
 
 func buildGetMemberByIDSuccess(w http.ResponseWriter, data interface{}) {
+	buildSuccessResponse(w, data)
+}
+
+func buildGetMemberListError(w http.ResponseWriter, err error) {
+	switch err {
+	case utils.ErrNoDocument, utils.ErrorInvalidPrimitiveID:
+		buildBadRequestResponse(w, err)
+	default:
+		buildInternalServerResponse(w, err)
+	}
+}
+
+func buildGetMemberListSuccess(w http.ResponseWriter, data interface{}) {
 	buildSuccessResponse(w, data)
 }
