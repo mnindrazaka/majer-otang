dcd3156bdb5035e22259d4068b69ac6f42df8c34
AndriP8 <andrikcoc26@gmail.com>
2022-08-06 21:25:08 +0700

    chore: adding context and typing payment machine

--- frontend/src/machines/paymentMachine.ts
+++ frontend/src/machines/paymentMachine.ts
@@ -1,8 +1,130 @@
-import { createMachine } from "xstate";
+import { assign, createMachine } from "xstate";
+import { Member } from "../__generated__/api";
+import { match } from "ts-pattern";
 
-const paymentMachine = createMachine({
+interface Context {
+  memberDetail: Member[];
+  memberDetailError: string | null;
+  payments: [];
+  paymentsError: string | null;
+  submitPayment: [];
+  submitPaymetError: string | null;
+}
+
+type MachineState =
+  | {
+      value: "idle";
+      context: Context & {
+        memberError: null;
+        paymentsError: null;
+        submitPaymetError: null;
+      };
+    }
+  | {
+      value: "fetchingMemberDetail";
+      context: Context & {
+        memberError: null;
+        paymentsError: null;
+      };
+    }
+  | {
+      value: "fetchingMemberDetailError";
+      context: Context & {
+        memberError: string;
+        paymentsError: null;
+      };
+    }
+  | {
+      value: "fetchingMemberDetailSucces";
+      context: Context & {
+        memberError: null;
+        paymentsError: null;
+        submitPaymetError: null;
+      };
+    }
+  | {
+      value: "fetchingPayment";
+      context: Context & {
+        memberError: null;
+        paymentsError: null;
+        submitPaymetError: null;
+      };
+    }
+  | {
+      value: "fetchingPaymentError";
+      context: Context & {
+        memberError: null;
+        paymentsError: string;
+      };
+    }
+  | {
+      value: "fetchingPaymentSuccess";
+      context: Context & {
+        memberError: null;
+        paymentsError: null;
+        submitPaymetError: null;
+      };
+    }
+  | {
+      value: "confirmingPayment";
+      context: Context & {
+        memberError: null;
+        paymentsError: null;
+        submitPaymetError: null;
+      };
+    }
+  | {
+      value: "submittingPayment";
+      context: Context & {
+        memberError: null;
+        paymentsError: null;
+        submitPaymetError: null;
+      };
+    }
+  | {
+      value: "submittingPaymentError";
+      context: Context & {
+        memberError: null;
+        paymentsError: null;
+        submitPaymetError: string;
+      };
+    };
+
+type MachineEvents =
+  | { type: "FETCH_MEMBER_DETAIL" }
+  | { type: "FETCH_MEMBER_DETAIL_SUCCESS"; memberDetailData: Member[] }
+  | { type: "FETCH_MEMBER_DETAIL_ERROR"; memberDetailErrorMessage: string }
+  | { type: "REFETCH_MEMBER_DETAIL" }
+  | { type: "FETCH_PAYMENT" }
+  | { type: "FETCH_PAYMENT_SUCCESS"; paymentsData: [] }
+  | { type: "FETCH_PAYMENT_ERROR"; paymentErrorMessage: string }
+  | { type: "REFETCH_PAYMENT" }
+  | { type: "SELECT_PAYMENT_TARGET" }
+  | { type: "CANCEL_PAYMENT" }
+  | { type: "CONFIRM_PAYMENT" }
+  | { type: "SUBMIT_PAYMENT_SUCCESS" }
+  | { type: "SUBMIT_PAYMENT_ERROR"; submitPaymentErrorMessage: string }
+  | { type: "RESUBMIT_PAYMENT" };
+
+export const paymentMachine = createMachine<
+  Context,
+  MachineEvents,
+  MachineState
+>({
   id: "Payment",
   initial: "idle",
+  schema: {
+    context: {} as Context,
+    events: {} as MachineEvents
+  },
+  context: {
+    memberDetail: [],
+    memberDetailError: null,
+    payments: [],
+    paymentsError: null,
+    submitPayment: [],
+    submitPaymetError: null
+  },
   states: {
     idle: {
       on: {
@@ -14,10 +136,12 @@ const paymentMachine = createMachine({
     fetchingMemberDetail: {
       on: {
         FETCH_MEMBER_DETAIL_SUCCESS: {
-          target: "fetchingMemberDetailSuccess"
+          target: "fetchingMemberDetailSuccess",
+          actions: "updateContext"
         },
         FETCH_MEMBER_DETAIL_ERROR: {
-          target: "fetchingMemberDetailError"
+          target: "fetchingMemberDetailError",
+          actions: "updateContext"
         }
       }
     },
@@ -38,10 +162,12 @@ const paymentMachine = createMachine({
     fetchingPayment: {
       on: {
         FETCH_PAYMENT_SUCCESS: {
-          target: "fetchingPaymentSuccess"
+          target: "fetchingPaymentSuccess",
+          actions: "updateContext"
         },
         FETCH_PAYMENT_ERROR: {
-          target: "fetchingPaymentError"
+          target: "fetchingPaymentError",
+          actions: "updateContext"
         }
       }
     },
@@ -72,10 +198,12 @@ const paymentMachine = createMachine({
     submittingPayment: {
       on: {
         SUBMIT_PAYMENT_ERROR: {
-          target: "submittingPaymentError"
+          target: "submittingPaymentError",
+          actions: "updateContext"
         },
         SUBMIT_PAYMENT_SUCCESS: {
-          target: "fetchingPayment"
+          target: "fetchingPayment",
+          actions: "updateContext"
         }
       }
     },
@@ -89,5 +217,41 @@ const paymentMachine = createMachine({
         }
       }
     }
+  }
+}).withConfig({
+  actions: {
+    updateContext: assign((ctx, event) =>
+      match(event)
+        .with({ type: "FETCH_MEMBER_DETAIL" }, () => ctx)
+        .with({ type: "FETCH_MEMBER_DETAIL_ERROR" }, (event) => ({
+          ...ctx,
+          memberDetailError: event.memberDetailErrorMessage
+        }))
+        .with({ type: "REFETCH_MEMBER_DETAIL" }, () => ctx)
+        .with({ type: "FETCH_MEMBER_DETAIL_SUCCESS" }, (event) => ({
+          ...ctx,
+          memberDetail: event.memberDetailData
+        }))
+        .with({ type: "FETCH_PAYMENT" }, () => ctx)
+        .with({ type: "FETCH_PAYMENT_ERROR" }, (event) => ({
+          ...ctx,
+          paymentsError: event.paymentErrorMessage
+        }))
+        .with({ type: "REFETCH_PAYMENT" }, () => ctx)
+        .with({ type: "FETCH_PAYMENT_SUCCESS" }, (event) => ({
+          ...ctx,
+          payments: event.paymentsData
+        }))
+        .with({ type: "SELECT_PAYMENT_TARGET" }, () => ctx)
+        .with({ type: "CANCEL_PAYMENT" }, () => ctx)
+        .with({ type: "CONFIRM_PAYMENT" }, () => ctx)
+        .with({ type: "SUBMIT_PAYMENT_SUCCESS" }, () => ctx)
+        .with({ type: "SUBMIT_PAYMENT_ERROR" }, (event) => ({
+          ...ctx,
+          submitPaymetError: event.submitPaymentErrorMessage
+        }))
+        .with({ type: "RESUBMIT_PAYMENT" }, () => ctx)
+        .exhaustive()
+    )
   }
 });
