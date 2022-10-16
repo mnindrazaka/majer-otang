e5268f396f1dc8dc90b55fe27fc26ac0b2003f89
AndriP8 <andrikcoc26@gmail.com>
2022-10-16 11:43:26 +0700

    fix: scroll on billing form second step

--- frontend/src/containers/Billings/BillingForm__SecondStep.tsx
+++ frontend/src/containers/Billings/BillingForm__SecondStep.tsx
@@ -73,7 +73,7 @@ const BillingFormSecondStep = ({ members, send, billingForm }: Props) => {
               Bill Equally
             </Checkbox>
           </Flex>
-          <VStack spacing="4" my="4">
+          <VStack spacing="4" my="4" h="lg" overflowY="scroll">
             {filteredMembers.map((member, index) => (
               <Box
                 key={member.id}
